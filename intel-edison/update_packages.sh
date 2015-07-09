fd08dd43b14aed8f7158b233028686dd5452a241
Andreas Antener <antener_a@gmx.ch>
2015-07-09 07:47:20 +0200

    adding script with update instructions for intel edison

---
+++ intel-edison/update_packages.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Updating packages from a source installation
+#
+# Following instructions from:
+# http://answers.ros.org/question/109656/maintaining-a-ros-source-install-add-packages-update-release/
+#
+
+# backup old package pointers
+mv indigo-ros_comm-wet.rosinstall indigo-ros_comm-wet.rosinstall_old
+# fetch latest package pointers
+rosinstall_generator ros_comm mavros --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo-ros_comm-wet.rosinstall
+# check which packages have changed
+diff indigo-ros_comm-wet.rosinstall indigo-ros_comm-wet.rosinstall_old
+
+# update .rosinstall
+sudo wstool merge -t src indigo-ros_comm-wet.rosinstall
+# fetch sources (you have to confirm the update for each package)
+sudo wstool up -t src
+
+# remove previously built artifacts
+# Note: this seemed to be required in my case to rebuild everything correctly,
+# else I encountered various issues with dependent libraries (at compile time and at runtime)
+mv build_isolated build_isolated_old
+mv devel_isolated devel_isolated_old
+
+# start compilation and have a beer
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release -DMAVLINK_DIALECT=pixhawk --install-space /home/ros/indigo
+
+
